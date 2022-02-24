67294c58bd816c423bb05cf1a43b21a14dbda262
谷宇维 <molixianggu@gmail.com>
2022-02-24 20:21:44 +0800

    feature(win divert): add read_ex async

--- windivert/src/lib.rs
+++ windivert/src/lib.rs
@@ -2,7 +2,7 @@
 #![warn(missing_docs)]
 /*!
 Wrapper arround [`windivert_sys`] ffi crate.
-*/
+ */
 
 /// WinDivert address data structures
 pub mod address;
@@ -88,7 +88,7 @@ pub enum CloseAction {
 
 /// Main wrapper struct around windivert functionalities.
 pub struct WinDivert {
-    handle: HANDLE,
+    pub handle: HANDLE,
     layer: WinDivertLayer,
     tls_idx: u32,
 }
@@ -190,7 +190,7 @@ impl WinDivert {
                 &mut addr,
             )
         }
-        .as_bool()
+            .as_bool()
         {
             buffer.truncate(packet_length as usize);
             Ok(WinDivertPacket {
@@ -205,131 +205,130 @@ impl WinDivert {
             }
         }
     }
+    //
+    // /// Batched blocking recv function.
+    // pub fn recv_ex(
+    //     &self,
+    //     buffer_size: usize,
+    //     packet_count: usize,
+    // ) -> Result<Option<Vec<WinDivertPacket>>, WinDivertError> {
+    //     let mut packet_length = 0;
+    //     let mut buffer = vec![0u8; buffer_size];
+    //
+    //     let mut addr_len = (ADDR_SIZE * packet_count) as u32;
+    //     let mut addr_buffer: Vec<WINDIVERT_ADDRESS> =
+    //         vec![WINDIVERT_ADDRESS::default(); packet_count];
+    //
+    //     if unsafe {
+    //         wd::WinDivertRecvEx(
+    //             self.handle,
+    //             buffer.as_mut_ptr() as *mut c_void,
+    //             buffer.len() as u32,
+    //             &mut packet_length,
+    //             0,
+    //             addr_buffer.as_mut_ptr(),
+    //             &mut addr_len,
+    //             std::ptr::null_mut() as *mut OVERLAPPED,
+    //         )
+    //     }
+    //     .as_bool()
+    //     {
+    //         addr_buffer.truncate((addr_len / ADDR_SIZE as u32) as usize);
+    //         buffer.truncate(packet_length as usize);
+    //         Ok(Some(self.parse_packets(buffer, addr_buffer)))
+    //     } else {
+    //         let err = WinDivertRecvError::try_from(std::io::Error::last_os_error());
+    //         match err {
+    //             Ok(err) => Err(WinDivertError::Recv(err)),
+    //             Err(err) => Err(WinDivertError::OSError(err)),
+    //         }
+    //     }
+    // }
+    //
+    // /// Bacthed recv function with timeout.
+    // pub fn recv_ex_wait(
+    //     &self,
+    //     buffer_size: usize,
+    //     timeout_ms: u32,
+    //     packet_count: usize,
+    // ) -> Result<Option<Vec<WinDivertPacket>>, WinDivertError> {
+    //     let mut packet_length = 0u32;
+    //     let mut buffer = vec![0u8; buffer_size * packet_count];
+    //
+    //     let mut overlapped: OVERLAPPED = unsafe { std::mem::zeroed() };
+    //     overlapped.hEvent = WinDivert::get_event(self.tls_idx)?;
+    //
+    //     let mut ioctl: WINDIVERT_IOCTL_RECV = unsafe { std::mem::zeroed() };
+    //     let mut addr_buffer: Vec<WINDIVERT_ADDRESS> =
+    //         vec![WINDIVERT_ADDRESS::default(); packet_count];
+    //     let mut addr_len = (std::mem::size_of::<WINDIVERT_ADDRESS>() * packet_count) as u32;
+    //     ioctl.addr = &mut addr_buffer[0] as *mut _ as u64;
+    //     ioctl.addr_len_ptr = &mut addr_len as *mut u32 as u64;
+    //     let res = unsafe {
+    //         DeviceIoControl(
+    //             self.handle,
+    //             IOControlCode::CreateIOControlCode(
+    //                 FILE_DEVICE_NETWORK as u16,
+    //                 0x923,
+    //                 IOControlAccessMode::Read,
+    //                 IOControlBufferingMethod::DirectOutput,
+    //             )
+    //                 .unwrap()
+    //                 .ControlCode()
+    //                 .unwrap(),
+    //             &mut ioctl as *mut _ as *mut c_void,
+    //             std::mem::size_of::<WINDIVERT_IOCTL_RECV>() as u32,
+    //             buffer.as_mut_ptr() as *mut c_void,
+    //             buffer.len() as u32,
+    //             &mut packet_length,
+    //             &mut overlapped,
+    //         )
+    //     };
+    //
+    //     if !res.as_bool()
+    //         && std::io::Error::last_os_error().raw_os_error().unwrap() as u32 == ERROR_IO_PENDING
+    //     {
+    //         loop {
+    //             let res = unsafe {
+    //                 GetOverlappedResultEx(
+    //                     self.handle,
+    //                     &mut overlapped,
+    //                     &mut packet_length,
+    //                     timeout_ms,
+    //                     true,
+    //                 )
+    //             };
+    //
+    //             if res.as_bool() {
+    //                 break;
+    //             } else {
+    //                 let return_cause =
+    //                     std::io::Error::last_os_error().raw_os_error().unwrap() as u32;
+    //                 match return_cause {
+    //                     WAIT_TIMEOUT => {
+    //                         unsafe { CancelIo(self.handle) };
+    //                         return Ok(None);
+    //                     }
+    //                     WAIT_IO_COMPLETION => break,
+    //                     value => {
+    //                         if let Ok(err) = WinDivertRecvError::try_from(value as i32) {
+    //                             return Err(WinDivertError::Recv(err));
+    //                         } else {
+    //                             panic!("This arm should never be reached")
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     addr_buffer.truncate((addr_len / ADDR_SIZE as u32) as usize);
+    //     buffer.truncate(packet_length as usize);
+    //     Ok(Some(self.parse_packets(buffer, addr_buffer)))
+    // }
 
-    /// Batched blocking recv function.
-    pub fn recv_ex(
+    pub fn read_ex(
         &self,
         buffer_size: usize,
-        packet_count: usize,
-    ) -> Result<Option<Vec<WinDivertPacket>>, WinDivertError> {
-        let mut packet_length = 0;
-        let mut buffer = vec![0u8; buffer_size];
-
-        let mut addr_len = (ADDR_SIZE * packet_count) as u32;
-        let mut addr_buffer: Vec<WINDIVERT_ADDRESS> =
-            vec![WINDIVERT_ADDRESS::default(); packet_count];
-
-        if unsafe {
-            wd::WinDivertRecvEx(
-                self.handle,
-                buffer.as_mut_ptr() as *mut c_void,
-                buffer.len() as u32,
-                &mut packet_length,
-                0,
-                addr_buffer.as_mut_ptr(),
-                &mut addr_len,
-                std::ptr::null_mut() as *mut OVERLAPPED,
-            )
-        }
-        .as_bool()
-        {
-            addr_buffer.truncate((addr_len / ADDR_SIZE as u32) as usize);
-            buffer.truncate(packet_length as usize);
-            Ok(Some(self.parse_packets(buffer, addr_buffer)))
-        } else {
-            let err = WinDivertRecvError::try_from(std::io::Error::last_os_error());
-            match err {
-                Ok(err) => Err(WinDivertError::Recv(err)),
-                Err(err) => Err(WinDivertError::OSError(err)),
-            }
-        }
-    }
-
-    /// Single packet recv with timout.
-    pub fn recv_wait(
-        &self,
-        buffer_size: usize,
-        timeout_ms: u32,
-    ) -> Result<Option<WinDivertPacket>, WinDivertError> {
-        let mut packet_length = 0u32;
-        let mut buffer = vec![0u8; buffer_size];
-
-        let mut overlapped: OVERLAPPED = unsafe { std::mem::zeroed() };
-        overlapped.hEvent = WinDivert::get_event(self.tls_idx)?;
-
-        let mut ioctl: WINDIVERT_IOCTL_RECV = unsafe { std::mem::zeroed() };
-        let mut addr: WINDIVERT_ADDRESS = unsafe { std::mem::zeroed() };
-        ioctl.addr = &mut addr as *mut _ as *mut c_void as u64;
-        ioctl.addr_len_ptr = std::ptr::null() as *const c_void as u64;
-
-        let res = unsafe {
-            DeviceIoControl(
-                self.handle,
-                IOControlCode::CreateIOControlCode(
-                    FILE_DEVICE_NETWORK as u16,
-                    0x923,
-                    IOControlAccessMode::Read,
-                    IOControlBufferingMethod::DirectOutput,
-                )
-                .unwrap()
-                .ControlCode()
-                .unwrap(),
-                &mut ioctl as *mut _ as *mut c_void,
-                std::mem::size_of::<WINDIVERT_IOCTL_RECV>() as u32,
-                buffer.as_mut_ptr() as *mut c_void,
-                buffer.len() as u32,
-                &mut packet_length,
-                &mut overlapped,
-            )
-        };
-
-        if !res.as_bool()
-            && std::io::Error::last_os_error().raw_os_error().unwrap() as u32 == ERROR_IO_PENDING
-        {
-            loop {
-                let res = unsafe {
-                    GetOverlappedResultEx(
-                        self.handle,
-                        &mut overlapped,
-                        &mut packet_length,
-                        timeout_ms,
-                        true,
-                    )
-                };
-                if res.as_bool() {
-                    break;
-                } else {
-                    let return_cause =
-                        std::io::Error::last_os_error().raw_os_error().unwrap() as u32;
-                    match return_cause {
-                        WAIT_TIMEOUT => {
-                            unsafe { CancelIo(self.handle) };
-                            return Ok(None);
-                        }
-                        WAIT_IO_COMPLETION => break,
-                        value => {
-                            if let Ok(err) = WinDivertRecvError::try_from(value as i32) {
-                                return Err(WinDivertError::Recv(err));
-                            } else {
-                                panic!("This arm should never be reached")
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        buffer.truncate(packet_length as usize);
-        Ok(Some(WinDivertPacket::from(WinDivertPacket {
-            address: addr,
-            data: buffer,
-        })))
-    }
-
-    /// Bacthed recv function with timeout.
-    pub fn recv_ex_wait(
-        &self,
-        buffer_size: usize,
-        timeout_ms: u32,
         packet_count: usize,
     ) -> Result<Option<Vec<WinDivertPacket>>, WinDivertError> {
         let mut packet_length = 0u32;
@@ -353,9 +352,9 @@ impl WinDivert {
                     IOControlAccessMode::Read,
                     IOControlBufferingMethod::DirectOutput,
                 )
-                .unwrap()
-                .ControlCode()
-                .unwrap(),
+                    .unwrap()
+                    .ControlCode()
+                    .unwrap(),
                 &mut ioctl as *mut _ as *mut c_void,
                 std::mem::size_of::<WINDIVERT_IOCTL_RECV>() as u32,
                 buffer.as_mut_ptr() as *mut c_void,
@@ -364,43 +363,12 @@ impl WinDivert {
                 &mut overlapped,
             )
         };
-
         if !res.as_bool()
             && std::io::Error::last_os_error().raw_os_error().unwrap() as u32 == ERROR_IO_PENDING
         {
-            loop {
-                let res = unsafe {
-                    GetOverlappedResultEx(
-                        self.handle,
-                        &mut overlapped,
-                        &mut packet_length,
-                        timeout_ms,
-                        true,
-                    )
-                };
-
-                if res.as_bool() {
-                    break;
-                } else {
-                    let return_cause =
-                        std::io::Error::last_os_error().raw_os_error().unwrap() as u32;
-                    match return_cause {
-                        WAIT_TIMEOUT => {
-                            unsafe { CancelIo(self.handle) };
-                            return Ok(None);
-                        }
-                        WAIT_IO_COMPLETION => break,
-                        value => {
-                            if let Ok(err) = WinDivertRecvError::try_from(value as i32) {
-                                return Err(WinDivertError::Recv(err));
-                            } else {
-                                panic!("This arm should never be reached")
-                            }
-                        }
-                    }
-                }
-            }
+            return Ok(None);
         }
+
         addr_buffer.truncate((addr_len / ADDR_SIZE as u32) as usize);
         buffer.truncate(packet_length as usize);
         Ok(Some(self.parse_packets(buffer, addr_buffer)))
